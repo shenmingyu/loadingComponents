de72dd1577d4aa53fe5c1b4154f4f5a87a9cb29e
shenmingyu <7502774+shenmingyu@user.noreply.gitee.com>
2020-10-30 20:37:41 +0800

    componentsUpdate1030

---
+++ waveball/style.js
@@ -0,0 +1,24 @@
+    const textInfo = document.getElementById("textInfo");
+    var text = "正在加载中.......";
+    console.log(text);
+
+    function setText(changeText) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          textInfo.innerHTML = changeText;
+          resolve();
+        }, 500);
+      });
+    }
+    async function main(flag) {
+      if (flag) {
+        for (let i = 5; i < text.length; i++) {
+          await setText(text.substr(0, i));
+        }
+      } else {
+        setText(text.substr(0, 5));
+      }
+
+      main(!flag);
+    }
+    main(1);
